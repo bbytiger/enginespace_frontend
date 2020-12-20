aa5934de25db49be29c9b33a386745b6187cc921
andy200212 <andy200212@gmail.com>
2020-12-20 01:28:30 -0800

    initla home page completed

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Greeting from './components/greeting.js'
+
+function App() {
+  return (
+    <Greeting />
+  );
+}
+
+export default App;
---
+++ src/components/greeting.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import LightLogo from '../assets/logo_light.png'
+import DarkLogo from '../assets/logo_dark.png'
+
+class Greeting extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      theme: "light"
+    }
+  }
+
+  render() {
+    return (
+      /* a theme switcher here */
+      <div className="greetings">
+        <div className="center_text">
+          <div className="escape_layout">
+            <h1 className="header">
+              EngineSpace
+            </h1>
+            <p className="catchphrase">The smartest bots on the internet</p>
+          </div>
+        </div>
+        <div className="logo_container">
+          <img src={this.state.theme === "light" ? LightLogo : DarkLogo} alt="EngineSpace Logo"/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Greeting
