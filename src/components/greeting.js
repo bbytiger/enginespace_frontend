3a82d58822bc4a3891e9678a5db2b166f705bc9f
andy200212 <andy200212@gmail.com>
2020-12-20 16:22:52 -0800

    finished greetings styling

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -3,7 +3,8 @@ import LightLogo from '../assets/logo_light.png'
 import DarkLogo from '../assets/logo_dark.png'
 import { RiSettings4Fill } from 'react-icons/ri';
 import { IoSearchSharp } from 'react-icons/io5';
-import { AiFillProfile } from 'react-icons/ai'
+import { AiFillProfile } from 'react-icons/ai';
+import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
 import { IconContext } from 'react-icons';
 //import { FormGroup, FormControlLabel, Switch } from '@material-ui/core';
 
@@ -22,7 +23,7 @@ class Greeting extends React.Component {
         <div className="greetings">
           <div className="center_text">
             <div className="escape_layout">
-              <h1 className="header">
+              <h1 className="header gradient_header">
                 EngineSpace
               </h1>
               <p className="catchphrase">The smartest bots on the internet</p>
@@ -35,30 +36,49 @@ class Greeting extends React.Component {
         <div className="control_panel">
           <div className="control_icon">
             <IconContext.Provider
-                value={{size: '2.3vmin', fontWeight: 500 }}
+                value={{size: '2.3vmin', fontWeight: 500, padding: 0 }}
               >
               <AiFillProfile />
             </IconContext.Provider>
           </div>
           <div className="control_icon">
             <IconContext.Provider
-                value={{size: '2.3vmin', fontWeight: 500 }}
+                value={{size: '2.3vmin', fontWeight: 500, padding: 0 }}
               >
               <IoSearchSharp />
             </IconContext.Provider>
           </div>
           <div className="control_icon">
             <IconContext.Provider
-              value={{size: '2.3vmin', fontWeight: 500 }}
+              value={{size: '2.3vmin', fontWeight: 500, padding: 0 }}
             >
               <RiSettings4Fill />
             </IconContext.Provider>
           </div>
         </div>
-        <div className="learn_more">
-          {/* <FormGroup row>
-          <FormControlLabel control={<Switch />}/>
-          </FormGroup> */}
+        <div className="learn_more down_bounce">
+          <p className="learn_more_text">Take a tour</p>
+          <div className="arrow" onClick={() => {
+            console.log("arrow was clicked")
+          }}>
+            <IconContext.Provider
+              value={{size: '6vmin', fontWeight: 500, padding: 0, opacity: 0.7}}
+            >
+              <IoIosArrowDown />
+            </IconContext.Provider>
+          </div>
+        </div>
+        <div className="registration side_bounce">
+          <span className="learn_more_text side_text">Take me in!</span>
+          <div className="arrow" onClick={() => {
+            console.log("arrow was clicked")
+          }}>
+            <IconContext.Provider
+              value={{size: '6vmin', fontWeight: 500, padding: 0, opacity: 0.7}}
+            >
+              <IoIosArrowForward />
+            </IconContext.Provider>
+          </div>
         </div>
       </div>
     )
